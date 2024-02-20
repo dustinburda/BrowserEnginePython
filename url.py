e7823c32de8407125d5e31ed4e38564ef12eb55b
dustinburda <dustin99wii@gmail.com>
2024-02-19 23:39:39 -0500

    Chapter1

--- url.py
+++ url.py
@@ -1,3 +1,4 @@
+import socket
 from dataclasses import dataclass
 
 @dataclass
@@ -6,6 +7,27 @@ class URL:
     host: str
     path: str
 
-    @staticmethod
+    @classmethod
     def new_url(url: str):
-        return URL(scheme="Hello", host="my name is", path="Dustin Burda")
+        scheme, url = url.split("://", 1)
+
+        if "/" not in url:
+            url = url + "/"
+
+        host, url = url.split("/", 1)
+        path = "/" + url
+
+        return URL(scheme=scheme, host=host, path=path)
+
+    def request(self):
+        s = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM, proto=socket.IPPROTO_TCP)
+        s.connect((self.host, 80))
+
+        s.send(f"GET {self.path} HTTP/1.0\r\nHost: {self.host}\r\n\r\n".encode("utf8"))
+
+
+
+
+
+
+
