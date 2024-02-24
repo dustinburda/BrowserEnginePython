e20e3bfe6e135dd7e514a47a4ffedf30dcdee365
dustinburda <dustin99wii@gmail.com>
2024-02-24 00:42:51 -0500

    Refactored Layout

--- browser.py
+++ browser.py
@@ -4,9 +4,9 @@ import tkinter.font
 from main import WIDTH, HEIGHT
 from url import lex
 from elements import Tag, Text
+from layout import Layout, H_STEP, V_STEP
 
-H_STEP:      int = 13
-V_STEP:      int = 18
+
 N_STEP:      int = 24
 SCROLL_STEP: int = 100
 
@@ -22,46 +22,6 @@ class Browser:
         self.window.bind("<Down>", self.scrolldown)
         self.window.bind("<Up>", self.scrollup)
 
-
-    def layout(self, tokens: list[str]) -> list[tuple[int, int, str]]:
-        # TODO: refactor this
-        display_list = []
-        cursor_x = H_STEP
-        cursor_y = V_STEP
-        weight = "normal"
-        style = "roman"
-        size = 16
-
-        for tok in tokens:
-            if isinstance(tok, Text):
-                for word in tok.text.split():
-                    font = tkinter.font.Font(size=size, weight=weight, slant=style)
-                    w = font.measure(word)
-
-                    if word == "\n":
-                        cursor_x = H_STEP
-                        cursor_y += font.metrics("linespace") * 1.25
-                        continue
-
-                    if cursor_x + w + H_STEP > WIDTH:
-                        cursor_x = H_STEP
-                        cursor_y += font.metrics("linespace") * 1.25
-
-                    display_list.append((cursor_x, cursor_y, word, font))
-                    cursor_x += w + font.measure(" ")
-            elif tok.tag == "i":
-                style="italic"
-            elif tok.tag == "/i":
-                style="roman"
-            elif tok.tag == "b":
-                weight="bold"
-            elif tok.tag == "/b":
-                weight="normal"
-
-
-        self.max = cursor_y
-        return display_list
-
     def draw(self):
         self.canvas.delete("all")
         for coord_x, coord_y, word, font in self.display_list:
@@ -74,7 +34,11 @@ class Browser:
     def load(self, url: str):
         body = url.request()
         tokens = lex(body)
-        self.display_list = self.layout(tokens)
+
+        layout = Layout(tokens)
+        self.display_list = layout.display_list
+        self.max = layout.max_scroll
+
         self.draw()
 
     def scrolldown(self, e):
--- layout.py
+++ layout.py
@@ -1,3 +1,57 @@
+H_STEP:      int = 13
+V_STEP:      int = 18
+
+from elements import Text, Tag
+import tkinter
+from main import WIDTH, HEIGHT
+
 class Layout:
     def __init__(self, tokens):
         self.display_list = []
+        self.cursor_x = H_STEP
+        self.cursor_y = V_STEP
+        self.weight = "normal"
+        self.style = "roman"
+        self.size = 16
+
+        for tok in tokens:
+            self.token(tok)
+
+        self.max_scroll = self.cursor_y
+
+    def token(self, tok: str):
+        if isinstance(tok, Text):
+            for word in tok.text.split():
+                self.word(word)
+        elif tok.tag == "i":
+            self.style = "italic"
+        elif tok.tag == "/i":
+            self.style = "roman"
+        elif tok.tag == "b":
+            self.weight = "bold"
+        elif tok.tag == "/b":
+            self.weight = "normal"
+        elif tok.tag == "small":
+            self.size -= 2
+        elif tok.tag == "/small":
+            self.size += 2
+        elif tok.tag == "big":
+            self.size += 4
+        elif tok.tag == "/big":
+            self.size -= 4
+
+    def word(self, word: str):
+        font = tkinter.font.Font(size=self.size, weight=self.weight, slant=self.style)
+        w = font.measure(word)
+
+        if word == "\n":
+            self.cursor_x = H_STEP
+            self.cursor_y += font.metrics("linespace") * 1.25
+            return
+
+        if self.cursor_x + w + H_STEP > WIDTH:
+            self.cursor_x = H_STEP
+            self.cursor_y += font.metrics("linespace") * 1.25
+
+        self.display_list.append((self.cursor_x, self.cursor_y, word, font))
+        self.cursor_x += w + font.measure(" ")
